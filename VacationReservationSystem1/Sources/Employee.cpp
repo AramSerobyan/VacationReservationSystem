993d439c43646c87913720c217e5949806404795
vahagn <vahagn@teamable.me>
2017-12-08 22:00:04 +0400

    added Employee.cpp

---
+++ VacationReservationSystem1/Sources/Employee.cpp
@@ -0,0 +1,15 @@
+#include <../Employee.hpp>
+
+int updateVacationDays(int num){
+
+    this.vacationDays = num; 
+    return 0;
+}
+
+int newNotification(int RequestID)
+{
+    this.mailbox.push_back(RequestID);
+    this.hasEmail = true;
+    return 0;
+}
+
