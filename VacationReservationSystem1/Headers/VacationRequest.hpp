5d11aa0fc297f258a15736bd005ad3f9f79b9369
vahagn <vahagn@teamable.me>
2017-12-08 19:13:55 +0400

    some new classes

--- VacationReservationSystem1/Headers/VacationRequest.hpp
+++ VacationReservationSystem1/Headers/VacationRequest.hpp
@@ -1,7 +1,18 @@
 #ifndef _VACATION_REQUEST_HPP_
 #define _VACATION_REQUEST_HPP_
 #include <string>
+#include <TimeInfo.hpp>
+#include <Department.hpp>
+#include <utility>
 class VacationRequest{
+private:
+	int vacationID;
+	std::pair<timeInfo, timeInfo> timeInterval;
+    Department department; 
+public:
+	int becomeEmpty(bool isCancelled);
+	int checkIntersection(std::pair<timeinfo, timeinfo>, bool *res)
+}
 
 
 #endif
