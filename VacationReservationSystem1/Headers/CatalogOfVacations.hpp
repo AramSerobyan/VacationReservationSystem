f685c9c0d295a25c7a782bcf84d4a7c9573af5e7
AramSerobyan <serobyanaram@yahoo.com>
2017-12-08 19:22:33 +0400

    Vacs

--- VacationReservationSystem1/Headers/CatalogOfVacations.hpp
+++ VacationReservationSystem1/Headers/CatalogOfVacations.hpp
@@ -5,11 +5,12 @@
 #include <string>
 #include <vector>
 #include "Vacation.hpp"
+#include "TimeInfo.hpp"
 
 class CatalogOfVacations 
 {
 public:
-	Vacation* getCompleted(time_t date);
+	Vacation* getCompleted(timeInfo date);
 	int load();
 
 private:
