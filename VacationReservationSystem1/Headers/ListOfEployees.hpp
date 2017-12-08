5844d91fc266638f5cb16786d065339674222a30
vahagn <vahagn@teamable.me>
2017-12-08 21:50:18 +0400

    commit

---
+++ VacationReservationSystem1/Headers/ListOfEployees.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef _LISTOFEMPLOYEES_HPP_
+#define _LISTOFEMPLOYEES_HPP_
+#include "Employee.hpp"
+#include <vector>
+class ListOfEmployees{ 
+
+public:
+	 Employee findEmployee(int ID);
+     int load();
+private: 
+	std::vector<Employee> listOfEmployees; 
+}
+#endif
+
